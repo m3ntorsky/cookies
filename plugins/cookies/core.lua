c1d91ec1fbbddb1f2d52c1819c1ebb753bf1ea11
m3ntorsky <matski.dev@gmail.com>
2025-03-03 10:01:13 +0100

    update version

--- plugins/cookies/core.lua
+++ plugins/cookies/core.lua
@@ -2,7 +2,7 @@ function GetPluginAuthor()
     return "Swiftly Solution"
 end
 function GetPluginVersion()
-    return "2.0.3"
+    return "2.0.4"
 end
 function GetPluginName()
     return "Cookies"
